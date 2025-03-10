ea8b298b1a365ed469b956dc4337844319da668a
Prasetyo Damongi <damongi.yermia@gmail.com>
2025-03-10 15:12:08 +0800

    Update Nama Website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,21 @@ const Header = () => {
     return (
         <header className="bg-gray-900 text-white p-4 sticky top-0 z-50">
             <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-3xl font-bold">Pras</h1>
+                <h1 className="text-3xl font-bold">pras</h1>
                 <nav>
-                    <ul className="flex space-x-4">
+                    <ul className="flex space-x-6">
                         <li>
-                            <a href="#about" className="hover:text-blue-300">
+                            <a href="#about" className="hover:text-gray-400">
                                 About Me
                             </a>
                         </li>
                         <li>
-                            <a href="#projects" className="hover:text-blue-300">
+                            <a href="#projects" className="hover:text-gray-400">
                                 My Porjects
                             </a>
                         </li>
                         <li>
-                            <a href="#contact" className="hover:text-blue-300">
+                            <a href="#contact" className="hover:text-gray-400">
                                 Contact
                             </a>
                         </li>
